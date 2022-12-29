a8d32b5aecf9fbc8bd0413fc3ac8e1505d277f98
Shaked Erinberg <shakede@wix.com>
2022-12-29 14:56:55 +0200

    import logo

--- src/_codux/boards/form-card/contact-us.board.tsx
+++ src/_codux/boards/form-card/contact-us.board.tsx
@@ -1,5 +1,6 @@
 import { createBoard } from '@wixc3/react-board';
 import { FormCard } from '../../../components/form-card/form-card';
+import logo from '../../../assets/logo.png';
 
 export default createBoard({
     name: 'Contact-Us Form',
