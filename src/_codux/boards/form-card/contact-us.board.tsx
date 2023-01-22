e2d37ca33234b93d1c03360b62ab69aa43bce85b
Cario Wix <cario@wix.com>
2023-01-22 20:33:12 +0200

    Cario/fixes (#11)
    * chore: fixes
    
    * chore:
    
    * chore
    
    * chore
    
    * chore
    
    * chore: modified sass colors' names to match the css one
    
    * chore: color name rgb to hex

--- src/_codux/boards/form-card/contact-us.board.tsx
+++ src/_codux/boards/form-card/contact-us.board.tsx
@@ -1,10 +1,9 @@
 import { createBoard } from '@wixc3/react-board';
 import { FormCard } from '../../../components/form-card/form-card';
-import logo from '../../../assets/logo.png';
 
 export default createBoard({
     name: 'Contact-Us Form',
-    Board: () => <FormCard formType="contact" logoURL={logo} />,
+    Board: () => <FormCard formType="contact" />,
     environmentProps: {
         windowHeight: 600,
         windowWidth: 800,
--- src/components/form-card/form-card.tsx
+++ src/components/form-card/form-card.tsx
@@ -1,11 +1,13 @@
 import type React from 'react';
 import { Elevation, H1, TextArea, Button, Card, InputGroup } from '@blueprintjs/core';
-import defaultLogoURL from '../../assets/logo.svg';
+import classNames from 'classnames';
+import logoURL from '../../assets/logo.svg';
 import styles from './form-card.module.scss';
 
 export interface FormCardProps {
     formType?: 'signin' | 'contact' | 'signup';
-    logoURL?: string;
+    className?: string;
+    children?: React.ReactNode;
 }
 
 /**
@@ -15,18 +17,18 @@ export interface FormCardProps {
  * Use this demo to get a feel for how easy and fun it is to create and edit components in Codux using Blueprint.js, a 3rd party React-based UI toolkit.
  *
  */
-export const FormCard: React.FC<FormCardProps> = ({
-    formType = 'signin',
-    logoURL = defaultLogoURL,
-}) => {
+export const FormCard = ({ className, children, formType = 'signin' }: FormCardProps) => {
     const submitCaption =
         formType === 'signin' ? 'Sign In' : formType === 'signup' ? 'Sign Up' : 'Send';
     const formTitle =
         formType === 'signin' ? 'Sign In' : formType === 'signup' ? 'Sign Up' : 'Contact Us';
 
     return (
-        <Card className={`${styles.card} ${styles.wrapper}`} elevation={Elevation.FOUR}>
-            <div className={`${styles.card} ${styles.header}`}>
+        <Card
+            className={classNames(styles.card, styles.wrapper, className)}
+            elevation={Elevation.FOUR}
+        >
+            <div className={classNames(styles.card, styles.header)}>
                 <img className={styles.logo} src={logoURL} />
                 <H1>{formTitle}</H1>
             </div>
@@ -37,8 +39,8 @@ export const FormCard: React.FC<FormCardProps> = ({
             ) : (
                 <TextArea />
             )}
-
-            <div className={`${styles.card} ${styles.submit}`}>
+            <div>{children}</div>
+            <div className={classNames(styles.card, styles.submit)}>
                 {formType !== 'contact' ? 'Forgot your Password?' : <span />}
                 <Button
                     text={submitCaption}
